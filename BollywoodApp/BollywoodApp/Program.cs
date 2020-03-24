dd2892274567ed360fb7e0905fc3d81ce8d0001d
Rutva Gajera <rutwapatel.99@gmail.com>
2020-03-24 19:12:22 +0530

    movie-actor task completed

---
+++ BollywoodApp/BollywoodApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //  Console.WriteLine("Hello World!");
+        Console.WriteLine("Welcome in Bollywood App...\n1. Movie\n2. Actor\nEnter Your Choice : ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Movie movie = new Movie();
+                break;
+            case 2:
+                Actor actor = new Actor();
+                break;
+            default:
+                Console.WriteLine("You Enter Invalid Choice \n Thank You for Visisting...");
+                Console.ReadLine();
+                break;
+        }
+    }
+}
+
